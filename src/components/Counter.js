9e2d4b6881d0cc337aad3dd0b726c7a6913b0373
jessicareinh <148791371+jessicareinh@users.noreply.github.com>
2024-01-09 16:56:30 +0100

    add different solution possibility

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,6 +4,10 @@ import { useState } from "react";
 export default function Counter() {
   const [count, setCount] = useState(0);
 
+  function add() {
+    setCount(count + 1);
+  }
+
   return (
     <div className="counter">
       <h1>Colored Number</h1>
